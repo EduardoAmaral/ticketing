00523098308b25e0dcc3fa6b4af9a6fcaa9e6a81
EduardoAmaral <eduardofsamaral@gmail.com>
2021-02-21 17:20:55 -0300

    Standardize error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import { json } from "body-parser";
 
 import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
 
 app.use(signUpRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
