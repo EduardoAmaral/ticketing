833ca269eed536666f326b97bcd2444a1f62e139
EduardoAmaral <eduardofsamaral@gmail.com>
2021-02-21 17:37:39 -0300

    Add custom error for not found routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
 app.use(signUpRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
