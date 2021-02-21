d21c7b35ccaf799f42b86fcad01597430d91583d
EduardoAmaral <eduardofsamaral@gmail.com>
2021-02-21 19:02:28 -0300

    Add mongoose into auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
@@ -17,6 +18,16 @@ app.all("*", () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  await mongoose.connect("mongodb://auth-mongo-srv:27017/authdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
