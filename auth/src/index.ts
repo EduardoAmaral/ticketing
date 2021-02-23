f717a9837154402266563ec4fb8f4cd5aad3a626
EduardoAmaral <eduardofsamaral@gmail.com>
2021-02-23 19:54:59 -0300

    [Auth] Create an retrieve JWT with logged user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,13 +2,24 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+
+app.set("trust proxy", true);
+
 app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(signUpRouter);
 
