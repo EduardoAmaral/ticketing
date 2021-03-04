e0491000588bbd40dbfff443f20fe15ac29eabc2
EduardoAmaral <eduardofsamaral@gmail.com>
2021-03-03 21:47:00 -0300

    [Common] Migrate common middlewares and error types to lib

---
+++ common/src/index.ts
@@ -0,0 +1,9 @@
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/not-found-error';
+export * from './errors/request-valitation-error';
+export * from './errors/unauthorized-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/validate-request';
