f634b286f8904807cfaacf78367dcbaa48f5cc88
EduardoAmaral <eduardofsamaral@gmail.com>
2021-03-04 22:16:25 -0300

    [Auth] Add mongo uri environment variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,20 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-import app from "./app";
+import app from './app';
 
 const start = async () => {
   if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET should be defined");
+    throw new Error('JWT_SECRET must be defined');
   }
 
-  await mongoose.connect("mongodb://auth-mongo-srv:27017/authdb", {
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   });
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log('Listening on port 3000');
   });
 };
 
