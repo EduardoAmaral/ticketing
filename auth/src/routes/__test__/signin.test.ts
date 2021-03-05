92d4efac3891787bf3991a83d2707d36b3e74a42
EduardoAmaral <eduardofsamaral@gmail.com>
2021-03-04 22:55:29 -0300

    [Auth] Refactoring signup tests to validate response status code

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -6,66 +6,56 @@ const SIGN_IN_ROUTE = '/api/users/signin';
 
 describe('SignIn Route', () => {
   it('returns 400 with empty email', async () => {
-    const response = await request(app)
-      .post(SIGN_IN_ROUTE)
-      .send({
-        password: '1',
-      })
-      .expect(400);
+    const response = await request(app).post(SIGN_IN_ROUTE).send({
+      password: '1',
+    });
 
-    expect(response.body[0].field).toBe('email');
-    expect(response.body[0].message).toBe('Email must be valid');
+    expect(response.status).toEqual(400);
+    expect(response.body[0].field).toEqual('email');
+    expect(response.body[0].message).toEqual('Email must be valid');
   });
 
   it('returns 400 with invalid email', async () => {
-    const response = await request(app)
-      .post(SIGN_IN_ROUTE)
-      .send({
-        email: 'asd',
-        password: '1',
-      })
-      .expect(400);
+    const response = await request(app).post(SIGN_IN_ROUTE).send({
+      email: 'asd',
+      password: '1',
+    });
 
-    expect(response.body[0].field).toBe('email');
-    expect(response.body[0].message).toBe('Email must be valid');
+    expect(response.status).toEqual(400);
+    expect(response.body[0].field).toEqual('email');
+    expect(response.body[0].message).toEqual('Email must be valid');
   });
 
   it('returns 400 with empty password', async () => {
-    const response = await request(app)
-      .post(SIGN_IN_ROUTE)
-      .send({
-        email: 'email@mail.com',
-      })
-      .expect(400);
+    const response = await request(app).post(SIGN_IN_ROUTE).send({
+      email: 'email@mail.com',
+    });
 
-    expect(response.body[0].field).toBe('password');
-    expect(response.body[0].message).toBe('Password must be provided');
+    expect(response.status).toEqual(400);
+    expect(response.body[0].field).toEqual('password');
+    expect(response.body[0].message).toEqual('Password must be provided');
   });
 
   it('fails when user does not exist', async () => {
-    const response = await request(app)
-      .post(SIGN_IN_ROUTE)
-      .send({
-        email: 'email@mail.com',
-        password: '1',
-      })
-      .expect(400);
+    const response = await request(app).post(SIGN_IN_ROUTE).send({
+      email: 'email@mail.com',
+      password: '1',
+    });
 
-    expect(response.body[0].message).toBe('Invalid credentials');
+    expect(response.status).toEqual(400);
+    expect(response.body[0].message).toEqual('Invalid credentials');
   });
 
   it('fails when an incorrect password is supplied', async () => {
     await signup();
 
-    const response = await request(app)
-      .post(SIGN_IN_ROUTE)
-      .send({
-        email: 'test@test.com',
-        password: 'wrong password',
-      })
-      .expect(400);
+    const response = await request(app).post(SIGN_IN_ROUTE).send({
+      email: 'test@test.com',
+      password: 'wrong password',
+    });
 
-    expect(response.body[0].message).toBe('Invalid credentials');
+    expect(response.status).toEqual(400);
+    expect(response.body[0].message).toEqual('Invalid credentials');
   });
 
   it('responds with a cookie when given valid credentials', async () => {
