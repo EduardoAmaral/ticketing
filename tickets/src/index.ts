32572cf4cfed6c56456e41512bda17159a0ca5c2
EduardoAmaral <eduardofsamaral@gmail.com>
2021-03-04 22:04:40 -0300

    [Tickets] Config project

---
+++ tickets/src/index.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+import app from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET should be defined");
+  }
+
+  await mongoose.connect("mongodb://tickets-mongo-srv:27017/authdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
