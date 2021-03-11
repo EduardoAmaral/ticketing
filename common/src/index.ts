6d890f0a3e766577ee4c9e594399c330d815a89c
EduardoAmaral <eduardofsamaral@gmail.com>
2021-03-10 22:22:47 -0300

    [Common] Publish forbidden error class

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,6 +3,7 @@ export * from './errors/custom-error';
 export * from './errors/not-found-error';
 export * from './errors/request-valitation-error';
 export * from './errors/unauthorized-error';
+export * from './errors/forbidden-error';
 
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
